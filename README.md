e458ec298df6f562acd2c0135beb8d0333ed6d43
Kirill Maslov <maslovks@gmail.com>
2017-11-22 02:15:38 +0300

    first commit

--- README.md
+++ README.md
@@ -1,22 +1,35 @@
-# Ближайшие бары
+# The closest bars
 
-[FIXME. Здесь будет описание проекта]
+This script shows the biggest, the smallest and the closest to user bar in Moscow.
 
-# Как запустить
+# How to run this script
 
-Скрипт требует для своей работы установленного интерпретатора Python версии 3.5
+Script should be interpreted by python3.5 and requires installed packages 'requests' and 'geopy'. 
 
-Запуск на Linux:
-
-```bash
-
-$ python bars.py # possibly requires call of python3 executive instead of just python
-# FIXME вывести пример ответа скрипта
-
+Quick installation of required packages is possible due requirements.txt.
+```
+pip install -r requirements.txt
 ```
 
-Запуск на Windows происходит аналогично.
+To run the script in Linux terminal, it is necessary to pass GPS coordinates of user as arguments.
+```bash
+$ python bars.py <user_longitude> <user_latitude>
+```
+
+## Example
+python bars.py 37.6242342 55.763443453
+* The smallest bar in Moscow is "Сушистор":
+	Михалковская улица, дом 8
+	Тел.: +7(495) 230-00-00
+
+* The biggest bar in Moscow is "Спорт бар «Красная машина»":
+	Автозаводская улица, дом 23, строение 1
+	Тел.: +7(905) 795-15-84
+
+* The closest bar for you is "Bubo Tutor Club and Gastropub":
+	улица Рождественка, дом 12/1, строение 1
+	Тел.: +7(499) 707-88-69
 
 # Цели проекта
 
-Код создан в учебных целях. В рамках учебного курса по веб-разработке - [DEVMAN.org](https://devman.org)
+The project is created for educational purposes for [DEVMAN.org](https://devman.org)
--- bars.py
+++ bars.py
@@ -1,21 +1,90 @@
-import json
+import requests
+from geopy.distance import vincenty as calc_distance
+import argparse
 
 
-def load_data(filepath):
-    pass
+class Bar:
+
+    def __init__(self, raw_bar_data):
+        self.name = raw_bar_data["properties"]["Attributes"]["Name"]
+        self.seats_count = (raw_bar_data['properties']['Attributes']
+                            ['SeatsCount'])
+        self.longitude = raw_bar_data['geometry']['coordinates'][0]
+        self.latitude = raw_bar_data['geometry']['coordinates'][1]
+        self.address = raw_bar_data['properties']['Attributes']['Address']
+        self.phone_number = (raw_bar_data['properties']['Attributes']
+                             ['PublicPhone'][0]["PublicPhone"])
+
+    def __str__(self):
+        return '"{name}":\n\t{addr}\n\tТел.: +7{tel}'.format(name=self.name,
+                                                        addr=self.address,
+                                                        tel=self.phone_number)
+
+    def calc_distance_to_certain_point(self, point_longitude, point_latitude):
+        distance = calc_distance((self.longitude, self.latitude),
+                                 (point_longitude, point_latitude)).kilometers
+        return distance
 
 
-def get_biggest_bar(data):
-    pass
+def load_api_key():
+    api_key_file_path = './key.txt'
+    with open(api_key_file_path) as file:
+        api_key = file.read().rstrip()
+        return api_key
 
 
-def get_smallest_bar(data):
-    pass
+def arguments_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('longitude', type=float,
+                        help='longitude of users current location')
+    parser.add_argument('latitude', type=float,
+                        help='latitude of users current location')
+    arguments = parser.parse_args()
+    return arguments.longitude, arguments.latitude
 
 
-def get_closest_bar(data, longitude, latitude):
-    pass
+def load_data():
+    request_params = {'api_key': load_api_key()}
+    request = requests.get('https://apidata.mos.ru/v1/datasets/1796/features',
+                           params=request_params)
+    decoded_data = request.json()
+    return decoded_data
+
+
+def get_list_of_bars(decoded_data):
+    list_of_bars = [Bar(raw_bar_data) for raw_bar_data
+                    in decoded_data["features"]]
+    return list_of_bars
+
+
+def get_biggest_bar(list_of_bars):
+    biggest_bar = max(list_of_bars, key=lambda bar: bar.seats_count)
+    return biggest_bar
+
+
+def get_smallest_bar(list_of_bars):
+    smallest_bar = min(list_of_bars, key=lambda bar: bar.seats_count)
+    return smallest_bar
+
+
+def get_closest_bar(list_of_bars, user_longitude, user_latitude):
+    closest_bar = min(list_of_bars,
+                      key=lambda bar:
+                      bar.calc_distance_to_certain_point(user_longitude,
+                                                         user_latitude))
+    return closest_bar
 
 
 if __name__ == '__main__':
-    pass
+    user_longitude, user_latitude = arguments_parser()
+
+    decoded_data = load_data()
+    list_of_bars = get_list_of_bars(decoded_data)
+
+    smallest_bar = get_smallest_bar(list_of_bars)
+    biggest_bar = get_biggest_bar(list_of_bars)
+    closest_bar = get_closest_bar(list_of_bars, user_longitude, user_latitude)
+
+    print('* The smallest bar in Moscow is {}\n'.format(smallest_bar))
+    print('* The biggest bar in Moscow is {}\n'.format(biggest_bar))
+    print('* The closest bar for you is {}\n'.format(closest_bar))
